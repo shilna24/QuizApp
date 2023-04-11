b96550fb112404239145d609d714d1a756812ae1
Shilna P <pshilsrihanat1@gmail.com>
2023-04-11 17:40:05 +0530

    Save high scores in localStorage

--- end.js
+++ end.js
@@ -2,6 +2,11 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScores=JSON.parse(localStorage.getItem('highScores'))||[];
+
+const MAX_HIGH_SCORE=5;
+
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -11,3 +16,13 @@ username.addEventListener('keyup', () => {
 saveHighScore = (e) => {
     e.preventDefault();
 };
+
+const score={
+    score:Math.floor(Math.random() * 100),
+    name:username.value
+}
+highScores.push(score);
+highScores.sort((a,b)=>b.score-a.score);
+highScores.splice(5)
+localStorage.setItem('highScores',JSON.stringify(highScores))
+window.location.assign('/')
